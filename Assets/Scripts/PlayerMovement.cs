612c5cebd6fe1f9bbc555be75e9745c64c88fe60
epord <pedro.balaguer@hotmail.com>
2019-08-02 22:00:49 -0300

    Basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    //public Animator animator;
+    public float movingSpeed = 100f;
+
+    float movingDirection = 0f;
+    bool jump = false;
+
+    public void onLanding()
+    {
+        //animator.SetBool("jumping", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movingDirection = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            //animator.SetBool("jumping", true);
+        }
+
+        //float lookingDirection = Input.GetAxisRaw("Vertical");
+        //animator.SetFloat("lookingY", lookingDirection);
+
+        //animator.SetFloat("speed", Mathf.Abs(movingDirection));
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(movingDirection * movingSpeed * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
